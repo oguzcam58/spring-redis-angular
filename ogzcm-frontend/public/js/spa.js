a1ca392dfa597b5fd1015af51f9f749c172926d5
Oguz Cam <oguzcam58@gmail.com>
2015-07-08 00:04:33 +0300

    Frontend improvements
    Recaptcha added.
    After creating a new book, fields should be set empty, it is fixed.
    Loading gif feature has been improved.

--- ogzcm-frontend/public/js/spa.js
+++ ogzcm-frontend/public/js/spa.js
@@ -1,4 +1,4 @@
-app = angular.module("libraryApp", ["ui.bootstrap"]);
+app = angular.module("libraryApp", ["ui.bootstrap", "vcRecaptcha"]);
 
 // Main controller runs when index html is loaded
 app.controller("IndexCtrl", function($scope, $modal, getAllBooks, commonFactory) {
@@ -39,6 +39,7 @@ app.controller("IndexCtrl", function($scope, $modal, getAllBooks, commonFactory)
     });
 
     deleteModalInstance.result.then(function (result) {
+      // Ok button clicked
       $scope.alerts = [];
       $scope.alerts.push({"type":"success",
                           "msg": book.name + " has been deleted successfully"});
@@ -55,7 +56,7 @@ app.controller("IndexCtrl", function($scope, $modal, getAllBooks, commonFactory)
         }
       });
     }, function () {
-
+      // Cancel button clicked
     });
   };
 
@@ -75,9 +76,11 @@ app.controller("IndexCtrl", function($scope, $modal, getAllBooks, commonFactory)
     });
 
     saveModalInstance.result.then(function (result) {
+      // Ok button clicked
       $scope.alerts = [];
       $scope.alerts.push({"type":"success",
                           "msg":result.message});
+      $scope.newBook = {id:0, name:"", author:""};
       // To refresh main list
       var getBookPromise = getAllBooks.getData();
       getBookPromise.then(function(result) {  // this is only run after $http completes
@@ -90,9 +93,8 @@ app.controller("IndexCtrl", function($scope, $modal, getAllBooks, commonFactory)
           $scope.alerts.push(commonFactory.getServerErrorMessage());
         }
       });
-
-    }, function () {
-
+    }, function(){
+      // Cancel button clicked
     });
   };
 });
@@ -102,6 +104,20 @@ app.controller('CreateUpdateCtrl', function ($scope, $modalInstance, getItems, s
   $scope.alerts = [];
   $scope.title = getItems.title;
   $scope.book = getItems.book;
+  $scope.response = null;
+  $scope.widgetId = null;
+
+  $scope.model = {
+      key: '6LeDKAkTAAAAAH4GCGAdAOPooGFzGDTyPapXE19T'
+  };
+
+  $scope.setResponse = function (response) {
+      $scope.response = response;
+  };
+
+  $scope.setWidgetId = function (widgetId) {
+      $scope.widgetId = widgetId;
+  };
 
   $scope.ok = function () {
     $scope.alerts = [];
@@ -111,8 +127,11 @@ app.controller('CreateUpdateCtrl', function ($scope, $modalInstance, getItems, s
        $scope.book.name == null || $scope.book.author == null) {
       $scope.alerts.push({"type":"danger",
                           "msg":"Book's info should be provided."});
-    } else {
-      var saveBookPromise = saveBook.getData($scope.book);
+    } else if($scope.response == null || $scope.response == "") {
+      $scope.alerts.push({"type":"danger",
+                          "msg":"Please fill the captcha"});
+    }else {
+      var saveBookPromise = saveBook.getData($scope.book, $scope.response);
       saveBookPromise.then(function(result) {  // this is only run after $http completes
         if(result.type == "Success"){
           $scope.books = result.books;
@@ -166,40 +185,41 @@ app.factory("getAllBooks", function($http, commonFactory) {
   var getData = function() {
     showLoadingGif();
     return $http.get(commonFactory.getBackendUrl() + "/getAllBooks").then(function(result){
-      hideLoadingGif();
       return result.data;
+    }).finally(function(){
+      hideLoadingGif();
     });
-    hideLoadingGif();
   };
   return { getData: getData };
 });
 
 // This factory creates or updates a book via RestFul saveBook Service
 app.factory("saveBook", function($http, commonFactory) {
-  var getData = function(book) {
+  var getData = function(book, response) {
     showLoadingGif();
-    return $http.get(commonFactory.getBackendUrl() + "/saveBook?book=" + JSON.stringify(book)).then(function(result){
-      hideLoadingGif();
+    return $http.get(commonFactory.getBackendUrl() + "/saveBook?book=" + JSON.stringify(book) + "&captcha=" + response).then(function(result){
       return result.data;
+    }).finally(function(){
+      hideLoadingGif();
     });
-    hideLoadingGif();
   };
   return { getData: getData };
 });
 
-// This factory creates a new book via RestFul createBook Service
+// This factory deletes selected book via RestFul deleteBook Service
 app.factory("deleteBook", function($http, commonFactory) {
   var getData = function(bookId) {
     showLoadingGif();
     return $http.get(commonFactory.getBackendUrl() + "/deleteBook?bookId=" + bookId).then(function(result){
-      hideLoadingGif();
       return result.data;
+    }).finally(function(){
+      hideLoadingGif();
     });
-    hideLoadingGif();
   };
   return { getData: getData };
 });
 
+// Common entries are being kept in scope of this factory
 app.factory("commonFactory", function(){
   var commonFactory = {};
   var backendUrl = "http://localhost:8080";
